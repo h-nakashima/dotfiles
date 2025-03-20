73a82cda468c36c5c98269eac9904fbdec3c6b27
h-nakashima <h-nakashima@users.noreply.github.com>
2025-03-21 01:22:44 +0900

    change exec env from bash to zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,29 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${(%):-%N}")";
 
 git pull origin main;
 
 function doIt() {
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".macos" \
-		--exclude ".macos_settings" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.zsh_profile;
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+    rsync --exclude ".git/" \
+          --exclude ".DS_Store" \
+          --exclude ".macos" \
+          --exclude ".macos_settings" \
+          --exclude "bootstrap.sh" \
+          --exclude "README.md" \
+          --exclude "LICENSE-MIT.txt" \
+          -avh --no-perms . ~;
+    source ~/.zsh_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+if [[ "$1" == "--force" || "$1" == "-f" ]]; then
+    doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+    read "reply?This may overwrite existing files in your home directory. Are you sure? (y/n) "
+    echo "";
+    if [[ "$reply" =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
 fi;
 unset doIt;
